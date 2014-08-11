ab2842dea3c2684596340d63c96d6b4cfc10a062
1337Wolfpack <aureussz@gmail.com>
2014-08-11 18:49:01 +0200

    fixed bugs and parsing rudp stuff
    fixed bugs and parsing rudp stuff from recieved packets

--- sendHidCommandsFbx.py
+++ sendHidCommandsFbx.py
@@ -48,28 +48,69 @@ if __name__ == '__main__':
     sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
     sock.connect((freebox.address,int(freebox.port)))
     sock.setblocking(0)
-
+    
     while True:
         input = raw_input("input an hex packet:")
-    	hexArray = re.findall('..',input)
-     	hexString = ""
-      	for hex in hexArray:
-    	   chrVal= chr(int(hex, 16))
-    	   hexString = hexString + chrVal  
+    	#hexArray = re.findall('..',input.replace(' ','').replace(':',''))
+     	hexString = input.replace(' ','').replace(':','')
+      	#for hex in hexArray:
+    	#   chrVal= chr(int(hex, 16))
+    	#   hexString = hexString + chrVal  
     	
  
-    	print "sent :" + repr(hexString)
+    	print "sent :" + hexString
     
-     	sock.send(hexString)
-    #02010000bd980000d8aec240
+     	sock.send(hexString.decode('hex'))
+        #02010000bd980000d8aec240
     	time.sleep(1)
         try:
             received = sock.recv(512)
         except socket.error:
             continue
     	#received = sock.recv(1024)
+    	
+    	
+#===============================================================================
+# Low-level protocol is peer-to-peer. As UDP is not connected, one of the two involved peers must send a packet first. Any of the two can do it. This packet is expected to be containing a RUDP_CMD_CONN_REQ command with reliable sequence number to a random value.. This packet should be reliable, i.e. imply retransmits in the sender's code.
+# Peer is expected to answer with an unreliable packet containing both an ACK and a RUDP_CMD_CONN_RSP packet. Its sequence number must be random as well. If the response packet is lost in transit, handshake will fail and must be started over. This is intended.
+# After these two packets are exchanged, connection is established. Each peer takes the sequence number it received in first packet as granted. This is only true for first packet.
+# 
+# On an established connection, 3 main types of packets may transit:
+# 
+# - Ping/Pong packets
+# 
+# - Noop packets
+# 
+# - Data packets
+#===============================================================================
+    	
+    	
+     	data= repr(received.encode("hex"))
+     	command = int(data[1:5])
+     	commands = { 0 : 'RUDP_CMD_NOOP',
+					 1 : 'RUDP_CMD_CLOSE',
+    	 			 2 : 'RUDP_CMD_CONN_REQ',
+    	 			 3 : 'RUDP_CMD_CONN_RSP',
+    	 			 4 : 'RUDP_CMD_PING',
+    	 			 5 : 'RUDP_CMD_PONG',
+    	 			 11 : 'RUDP_CMD_APP'
+    	 			 }
+     	opts = { 1 : 'ACK',
+				 2 : 'RELIABLE',
+    	 		 4 : 'RETRANSMITTED' }
      	
-     	print "Received {}".format(repr(received))
+    	 			
+    		
+     	
+     	print "recieved :" +data
+     	print "command :" + data[1:3] + commands[int(data[1:3])]
+     	print "opt :" + data[3:5] + opts[int(data[3:5])]
+     	#print "data (hid?) :"+ data[5:]
+     	print "lookds like a device id :" + data[5:9]+ ":" + data [9:13]
+     	print "report id : " + data[13:17]
+     	
+     	
+     	
     
     
     sock.close()
