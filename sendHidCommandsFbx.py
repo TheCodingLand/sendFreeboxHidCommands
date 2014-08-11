adf43dfa5ca74bfd79c8d9cd0fa919b90a655627
1337Wolfpack <aureussz@gmail.com>
2014-08-11 19:17:15 +0200

    added FOILS_STATES

--- sendHidCommandsFbx.py
+++ sendHidCommandsFbx.py
@@ -23,6 +23,18 @@ class MyListener(object):
             prop = info.getProperties()
 
 
+FOILS_STATES = { 0 : 'FOILS_HID_DEVICE_NEW', # client to server
+    			 1 : 'FOILS_HID_DEVICE_DROPPED', # client to server
+	       		 2 : 'FOILS_HID_DEVICE_CREATED', # server to client
+    	      	 3 : 'FOILS_HID_DEVICE_CLOSE', # server to client
+    			 4 : 'FOILS_HID_FEATURE', # bidir
+        		 5 : 'FOILS_HID_DATA', # bidir
+     	     	 6 : 'FOILS_HID_GRAB', # server to client
+     	 	     7 : 'FOILS_HID_RELEASE', # server to client
+    	 	 	 8 : 'FOILS_HID_FEATURE_SOLLICIT' } # server to client
+
+
+
 COMMANDS = { 0 : 'RUDP_CMD_NOOP',
 			 1 : 'RUDP_CMD_CLOSE',
     	 	 2 : 'RUDP_CMD_CONN_REQ',
@@ -92,8 +104,10 @@ if __name__ == '__main__':
     	#   hexString = hexString + chrVal  
     	
     	parseRudpPacket(hexString)
-    	
     	print "sent :" + hexString
+    	print "----------------------------------------------------------------"
+    	
+    	
     	
     
      	sock.send(hexString.decode('hex'))
